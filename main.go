83a331f1a890126a4588bb02c98a1f45fca95f2c
kushal tamang <kushaltamang0926@gmail.com>
2026-02-24 21:43:13 +0545

    adding sqlite

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
+	fmt.Println("making an pomodoro app")
+	// main page
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl, err := template.ParseFiles("templates/index.html")
 		if err != nil {
@@ -14,6 +18,16 @@ func main() {
 		}
 		tmpl.Execute(w, nil)
 
+	})
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "<h1>helllo from go </h1>")
+	})
+
+	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
+		currentTime := time.Now()
+		formatted := currentTime.Format("2006-01-02 15:04:05")
+		fmt.Fprint(w, formatted)
 	})
 
 	http.ListenAndServe(":8080", nil)
