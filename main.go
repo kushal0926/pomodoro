0143fcd773edb7b229312ba6c8995bedee6eaafb
kushal tamang <kushaltamang0926@gmail.com>
2026-02-26 18:57:04 +0545

    added session store

--- main.go
+++ main.go
@@ -1,27 +1,35 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"time"
 
+	"github.com/joho/godotenv"
 	"github.com/kushal0926/pomodoro/db"
+	"github.com/kushal0926/pomodoro/handlers"
 	"github.com/kushal0926/pomodoro/middleware"
 )
 
 func main() {
+	godotenv.Load()
 	db.InitDB()
 	fmt.Println("making an pomodoro app")
+	gob.Register(int64(0))
+	gob.Register(int(0))
+	gob.Register(bool(false))
+
 	// main page
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl, err := template.ParseFiles("templates/index.html")
 		if err != nil {
 			log.Fatalf("failed to load template: %v", err)
 		}
+		session := middleware.GetSession(w, r)
 		tmpl.Execute(w, nil)
-		session :=middleware.GetSession(w, r)
 		log.Println(session)
 
 	})
@@ -35,6 +43,9 @@ func main() {
 		formatted := currentTime.Format("2006-01-02 15:04:05")
 		fmt.Fprint(w, formatted)
 	})
+
+	http.HandleFunc("/timer", handlers.GetTimer)
+	http.HandleFunc("/timer/start", handlers.StartTimer)
 
 	http.ListenAndServe(":8080", nil)
 }
--- middleware/session.go
+++ middleware/session.go
@@ -7,9 +7,10 @@ import (
 	"github.com/gorilla/sessions"
 )
 
-var store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
+var store *sessions.CookieStore
 
 func GetSession(w http.ResponseWriter, r *http.Request) *sessions.Session {
+	var store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
 	session, err := store.Get(r, "session-name")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
